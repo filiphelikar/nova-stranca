a595a00ad533510899fef97800dcfbcdfa5afe6f
CosmicCatCoder <clssmgs993@vigoneo.com>
2024-07-03 19:24:08 +0200

    remove unused inport

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import HomePage from './HomePage'
+import React from "react";
+import HomePage from "./HomePage";
 import { Metadata } from "next";
-import Head from 'next/head'
 
 export const metadata: Metadata = {
   title: "Filip Helikar | Home",
   description: "Home Filip Helikar",
-  other: 
-    {
-     "google-site-verification": "MM4TstpXUw18nxudSmty14AZ5vI2ZzUqoeJAiciNxVc"
-    }
+  other: {
+    "google-site-verification": "MM4TstpXUw18nxudSmty14AZ5vI2ZzUqoeJAiciNxVc",
+  },
 };
 
 const Page = () => {
@@ -17,9 +15,7 @@ const Page = () => {
     <>
       <HomePage />
     </>
-  )
-}
+  );
+};
 
-export default Page
-
-
+export default Page;
