1f07bbb372d04865ddafe0973768b8b7a9a17c92
CosmicCatCoder <clssmgs993@vigoneo.com>
2024-07-03 14:39:58 +0200

    google fix

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import HomePage from './HomePage'
 import { Metadata } from "next";
+import Head from 'next/head'
 
 export const metadata: Metadata = {
   title: "Filip Helikar | Home",
   description: "Home Filip Helikar",
+  other: 
+    {
+     "google-site-verification": "MM4TstpXUw18nxudSmty14AZ5vI2ZzUqoeJAiciNxVc"
+    }
 };
 
 const Page = () => {
