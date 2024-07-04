ac634b3a0ac5583e2c03eb15ea438a4868a22cf7
CosmicCatCoderNotebook <filip.helikar@seznam.cz>
2024-07-04 14:44:00 +0200

    font fix

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -5,9 +5,6 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Filip Helikar | Home",
   description: "Home Filip Helikar",
-  other: {
-    "google-site-verification": "MM4TstpXUw18nxudSmty14AZ5vI2ZzUqoeJAiciNxVc",
-  },
 };
 
 const Page = () => {
